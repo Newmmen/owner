25db9470b13fd48102e3ac77312061339cc2a204
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-11-03 08:36:55 +0300

    refactor

--- src/test/java/ga/guru/config/TestBase.java
+++ src/test/java/ga/guru/config/TestBase.java
@@ -1,25 +1,28 @@
 package ga.guru.config;
 
 import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class TestBase extends TestBaseConfig {
+public class TestBase {
+
+    static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
 
     @BeforeAll
     static void webConfig() {
-        TestBaseConfig webConfig = new TestBaseConfig();
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         Configuration.browserCapabilities = capabilities;
-        if (webConfig.get().isRemote()) {
+        if (config.isRemote()) {
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
-            Configuration.remote = webConfig.get().getSelenoidUrl();
+            Configuration.remote = config.getSelenoidUrl();
 
         }
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-        Configuration.browser = webConfig.get().getBrowserName();
-        Configuration.browserVersion = webConfig.get().getBrowserVersion();
+        Configuration.browser = config.getBrowserName();
+        Configuration.browserVersion = config.getBrowserVersion();
 
     }
 }
